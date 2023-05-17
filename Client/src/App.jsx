3a6c7f0e7865e22a58323155595b12729e893cb0
Naomi Cherotich <naomirono1454@gmail.com>
2023-05-17 16:25:08 +0300

    Import about component and render it

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,6 @@
 import NavbarLanding from './components/NavbarLanding';
 import LandingPage from './components/LandingPage';
+import About from './components/About';
 import './App.css'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className='bg-gradient-to-l from-[#dc2430] to-[#7B1FA2]'>
     <NavbarLanding/>
     <LandingPage/>
+    <About/>
     </div>
       
     </>
