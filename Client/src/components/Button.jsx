d832038887219e72ec6c800cd7f705c6d8957cfc
Naomi Cherotich <naomirono1454@gmail.com>
2023-05-17 16:03:52 +0300

    Remove the margin left in md screens

--- Client/src/components/Button.jsx
+++ Client/src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = (props) => {
   return (
-   <button className='bg-[#7B1FA2] text-white py-2 px-4 rounded-3xl md:ml-8 hover:bg-[#7B1FA2] duration-500 border border-white'>
+   <button className='bg-[#7B1FA2] text-white py-2 px-4 rounded-3xl hover:bg-[#7B1FA2] duration-500 border border-white'>
        {props.children}
    </button>
   )
