aab542788f6871923068450b9e2a317ef5a86db6
Naomi Cherotich <naomirono1454@gmail.com>
2023-05-17 19:47:23 +0300

    Create an impact section to display the impact made

---
+++ Client/src/components/ImpactSection.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Empowered from '../assets/empowered.JPG';
+import Schools from '../assets/schools.JPG';
+
+
+const ImpactSection = () => {
+  return (
+    <section className="py-16 bg-[#fff] max-w-[1200px] mx-auto">
+      <div className="container mx-auto">
+        <h2 className="text-3xl md:text-4xl font-bold text-center mb-12">Our Impact</h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+          <div className="text-center">
+            <img src={Schools} alt="Girls Empowered" className="mx-auto mb-4 w-26 h-26 " />
+            <h3 className="text-2xl font-bold mb-2">Schools Reached</h3>
+            <p className="text-gray-800 font-bold">100+</p>
+          </div>
+          <div className="text-center">
+            <img src={Empowered} alt="Schools Reached" className="mx-auto mb-4 w-26 h-26 " />
+            <h3 className="text-2xl font-bold mb-2">Girls Empowered</h3>
+            <p className="text-gray-800 font-bold">5000+</p>
+          </div>
+          <div className="text-center">
+            <img src={Schools} alt="Pads Distributed" className="mx-auto mb-4 w-26 h-26 " />
+            <h3 className="text-2xl font-bold mb-2">Pads Distributed</h3>
+            <p className="text-gray-800 font-bold">5000+</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ImpactSection;
