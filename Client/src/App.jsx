d287fd139b56d18d9748672d81a233c490ffc8f6
Naomi Cherotich <naomirono1454@gmail.com>
2023-05-17 13:28:40 +0300

    Create a Donation Form component

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import NavbarLanding from './components/NavbarLanding';
+import LandingPage from './components/LandingPage';
 
 function App() {
 
   return (
     < > 
-    <div>
+    <div className='bg-gradient-to-l from-[#dc2430] to-[#7B1FA2]'>
     <NavbarLanding/>
-   
+    <LandingPage/>
     </div>
       
     </>
--- Client/src/components/NavbarLanding.jsx
+++ Client/src/components/NavbarLanding.jsx
@@ -1,28 +1,34 @@
 import React from 'react'
+import Button from './Button';
+import Logo from '../assets/carelogo.png'
 
 const NavbarLanding = () => {
   return (
     <div>
-        <header className="bg-gray-100 max-w-[1280px] mx-auto">
-        <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <header className="">
+        <nav className="max-w-7xl mx-auto pt-2 pb-2 px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center">
               <div className="flex-shrink-0">
-                {/* <img className="h-8 w-8" src={logo} alt="Logo" /> */}
-              </div>
-              <div className="hidden md:block">
-                <h1 className="font-bold text-gray-800 text-xl">CareCycle</h1>
-                <p className="text-gray-500 text-sm">Providing sanitary towels to girls in need</p>
+                <img className="h-16 w-22" src={Logo} alt="Logo" />
               </div>
             </div>
-            <div className="hidden md:block">
-              <a href="#" className="font-medium text-gray-500 hover:text-gray-900 px-3 py-2 rounded-md text-sm">
+            <div className="hidden md:block md:flex md:items-center md:pb-0 pb-14 absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto
+        md-pl-0 pl-9 transition-all duration-500 ease-in">
+              <a href="#" className="font-medium text-white hover:text-gray-100 px-3 py-2 rounded-md md:ml-8 text-[16px] md:my-0 my-10">
+                Home
+              </a>
+              <a href="#" className="font-medium text-white hover:text-gray-100 px-3 py-2 rounded-md md:ml-8 text-[16px] md:my-0 my-10">
+                Our Products 
+              </a>
+              <a href="#" className="font-medium text-white hover:text-gray-100 px-3 py-2 rounded-md md:ml-8 text-[16px] md:my-0 my-10">
+                Events
+              </a>
+              <a href="#" className="font-medium text-white hover:text-gray-100 px-3 py-2 rounded-md md:ml-8 text-[16px] md:my-0 my-10">
                 Shop
               </a>
-              <a href="#" className="font-medium text-gray-500 hover:text-gray-900 px-3 py-2 rounded-md text-sm">
-                Donate
-              </a>
             </div>
+            <Button>Donate Now</Button>
           </div>
         </nav>
       </header>
