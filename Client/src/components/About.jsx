6797b981c8eebf52be804e08a43518a3c03a3024
Naomi Cherotich <naomirono1454@gmail.com>
2023-05-17 17:08:38 +0300

    Change background image, add a border to image, replace button

--- Client/src/components/About.jsx
+++ Client/src/components/About.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import Image from '../assets/About.jpg'
+import Button from './Button';
 
 const About = () => {
   return (
-    <section className="py-2 bg-gray-100">
+    <section className="py-2 bg-[#fff]">
       <div className="container mx-auto flex flex-col md:flex-row items-center justify-between">
         <div className="md:w-1/2 mb-8 md:mb-0">
+          <div className="border-4 border-purple-700">
           <img src={Image} alt="About Image" className="w-full" />
+          </div>
         </div>
         <div className="md:w-1/2 md:pl-8">
           <h2 className="text-3xl md:text-4xl font-bold mb-4">About CareCycle</h2>
           <p className="text-gray-800 mb-6">CareCycle is a non-profit organization dedicated to providing sanitary pads to girls and women in need. We believe that access to menstrual hygiene products is a basic right, and our mission is to ensure that no one is left without the necessary supplies to manage their periods with dignity.</p>
-          <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
-            Read More
-          </button>
+          <Button>Read More</Button>
         </div>
       </div>
     </section>
