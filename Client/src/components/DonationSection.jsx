bace99802ddf0e93dc7cb26ac012dc92fd2f717b
Naomi Cherotich <naomirono1454@gmail.com>
2023-05-18 15:00:37 +0300

    Add a background image

--- Client/src/components/DonationSection.jsx
+++ Client/src/components/DonationSection.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Button from './Button';
+import Image from '../assets/donation.jpg'
 
 const DonationSection = () => {
   return (
-    <section className="py-16 bg-gradient-to-l from-[#dc2430] to-[#7B1FA2]">
-      <div className="container mx-auto text-center">
-        <h2 className="text-3xl md:text-4xl font-bold mb-4 text-white">Make a Donation</h2>
-        <p className="text-gray-800 mb-6 text-gray-100">Your contribution can make a real difference in the lives of girls and women in need.</p>
+    <section className="  bg-cover bg-no-repeat" style={{ backgroundImage: `url(${Image})` }}>
+      <div className=" py-16 mx-auto text-center bg-white bg-opacity-50">
+        <h2 className="text-3xl md:text-4xl font-bold mb-4 text-black">Make a Donation</h2>
+        <p className="text-gray-800 mb-6 text-gray-100 text-black">Your contribution can make a real difference in the lives of girls and women in need.</p>
         <Button>Donate Now</Button>
       </div>
     </section>
