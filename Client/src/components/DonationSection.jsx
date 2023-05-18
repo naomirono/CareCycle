9632bcba6a4e4bf2d014e1e72407b52ffc531c52
Naomi Cherotich <naomirono1454@gmail.com>
2023-05-18 20:58:14 +0300

    Create a new div and add the h2, p and button

--- Client/src/components/DonationSection.jsx
+++ Client/src/components/DonationSection.jsx
@@ -5,10 +5,16 @@ import Image from '../assets/donation.jpg'
 const DonationSection = () => {
   return (
     <section className="  bg-cover bg-no-repeat" style={{ backgroundImage: `url(${Image})` }}>
-      <div className=" py-16 mx-auto text-center bg-white bg-opacity-50">
-        <h2 className="text-3xl md:text-4xl font-bold mb-4 text-black">Make a Donation</h2>
-        <p className="text-gray-800 mb-6 text-gray-100 text-black">Your contribution can make a real difference in the lives of girls and women in need.</p>
+      <div className=" py-16  text-center bg-white bg-opacity-50">
+      
+      <div className='max-w-[1000px] mx-auto'>
+        <h2 className="text-3xl md:text-4xl font-bold mb-4 text-black">Donate to Support Our Cause</h2>
+        <p className="text-gray-800 mb-6 text-gray-100 text-black text-center">
+          Together, we can empower women and break the barriers of menstrual stigma. Your support can provide access to sanitary pads and promote menstrual hygiene education, making a real difference in the lives of girls and women in need.
+        </p>
         <Button>Donate Now</Button>
+      </div>
+        
       </div>
     </section>
   );
