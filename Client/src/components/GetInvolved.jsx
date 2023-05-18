a688ad7b053036b7c0ebd74e15e3ac8aeabd2254
Naomi Cherotich <naomirono1454@gmail.com>
2023-05-18 21:26:26 +0300

    Add margin title and delete the contact form

--- Client/src/components/GetInvolved.jsx
+++ Client/src/components/GetInvolved.jsx
@@ -9,7 +9,7 @@ const GetInvolved = () => {
   return (
     <section className="bg-gray-100 py-10">
       <div className="container mx-auto">
-        <h2 className="text-3xl font-bold text-center mb-6">
+        <h2 className="text-3xl font-bold text-center mb-12">
           Get Involved
         </h2>
         <div className="flex flex-wrap justify-center">
@@ -61,55 +61,6 @@ const GetInvolved = () => {
               </p>
             </div>
           </div>
-        </div>
-        <div className="mt-8">
-          <h3 className="text-xl font-semibold mb-4">
-            Contact Us
-          </h3>
-          <form onSubmit={handleSubmit} className="max-w-md mx-auto">
-            <div className="mb-4">
-              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
-                Name
-              </label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                className="w-full px-4 py-2 border border-gray-400 rounded-md focus:outline-none focus:border-blue-400"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                className="w-full px-4 py-2 border border-gray-400 rounded-md focus:outline-none focus:border-blue-400"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="message">
-                Message
-              </label>
-              <textarea
-                id="message"
-                name="message"
-                rows="4"
-                className="w-full px-4 py-2 border border-gray-400 rounded-md focus:outline-none focus:border-blue-400"
-                required
-              ></textarea>
-            </div>
-            <button
-              type="submit"
-              className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md focus:outline-none"
-            >
-              Submit
-            </button>
-          </form>
         </div>
       </div>
     </section>
