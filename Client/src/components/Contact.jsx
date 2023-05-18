a03e31eeef05f1a379e148acf53004a4d4425744
Naomi Cherotich <naomirono1454@gmail.com>
2023-05-18 15:48:22 +0300

    Replace imported social icons

--- Client/src/components/Contact.jsx
+++ Client/src/components/Contact.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import { FaEnvelope, FaPhoneAlt } from 'react-icons/fa';
-import { AiFillTwitterCircle, AiFillInstagram } from 'react-icons/ai';
+import { FaEnvelope, FaPhoneAlt, FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 const ContactSection = () => {
   return (
-    <section className="py-16 bg-white">
+    <section className="py-16 bg-white max-w-[1200px] mx-auto">
       <div className="container mx-auto">
         <div className="flex flex-col md:flex-row">
           <div className="md:w-1/2 mb-8 md:mb-0">
             <h2 className="text-3xl md:text-4xl font-bold mb-4">Contact Us</h2>
-            <p className="text-gray-800 mb-6">Have questions or want to get involved? We'd love to hear from you. Reach out to us using the contact details below or fill out the form and we'll get back to you as soon as possible.</p>
+            <p className="text-gray-800 mb-6 pr-10">Have questions or want to get involved? We'd love to hear from you. Reach out to us using the contact details below or fill out the form and we'll get back to you as soon as possible.</p>
             <ul className="text-gray-800">
-              <li><FaEnvelope className="inline-block mr-2" /> Email: info@carecycle.org</li>
-              <li><FaPhoneAlt className="inline-block mr-2" /> Phone: +1234567890</li>
-              <li><AiFillTwitterCircle className="inline-block mr-2" /> Twitter: @carecycle</li>
-              <li><AiFillInstagram className="inline-block mr-2" /> Instagram: @carecycle</li>
+              <li className="mb-2"><FaEnvelope className="inline-block mr-2" /> Email: info@carecycle.org</li>
+              <li className="mb-2"><FaPhoneAlt className="inline-block mr-2" /> Phone: +1234567890</li>
+              <li className="mb-2"><FaTwitter className="inline-block mr-2 text-blue-500 hover:text-blue-700 cursor-pointer" /> Twitter: @carecycle</li>
+              <li className="mb-2"><FaFacebook className="inline-block mr-2 text-blue-500 hover:text-blue-700 cursor-pointer" /> Facebook: @carecycle</li>
+              <li><FaInstagram className="inline-block mr-2 text-pink-500 hover:text-pink-700 cursor-pointer" /> Instagram: @carecycle</li>
             </ul>
           </div>
           <div className="md:w-1/2">
@@ -31,7 +31,7 @@ const ContactSection = () => {
                 <label htmlFor="message" className="block text-gray-800 font-medium mb-2">Message</label>
                 <textarea id="message" className="border border-gray-400 py-2 px-4 rounded w-full h-32"></textarea>
               </div>
-              <button type="submit" className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+              <button type="submit" className="bg-[#7B1FA2] hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
                 Submit
               </button>
             </form>
