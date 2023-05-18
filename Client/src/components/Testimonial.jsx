8910d65859a9ab1dea8c9c45d4d8599578f6b6e8
Naomi Cherotich <naomirono1454@gmail.com>
2023-05-18 15:02:38 +0300

    Add a max-width, change text color and change section background color

--- Client/src/components/Testimonial.jsx
+++ Client/src/components/Testimonial.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const TestimonialsSection = () => {
   return (
-    <section className="py-16 bg-gray-100">
-      <div className="container mx-auto">
-        <h2 className="text-3xl md:text-4xl font-bold text-center mb-6">Testimonials</h2>
+    <section className="py-16 bg-gradient-to-l from-[#dc2430] to-[#7B1FA2]">
+      <div className="container max-w-[1200px] mx-auto">
+        <h2 className="text-3xl md:text-4xl font-bold text-center mb-6 text-white">Testimonials</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           <div className="bg-white p-6 rounded shadow">
             <p className="text-gray-800 mb-4">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae justo non augue feugiat sagittis. Aliquam ullamcorper rhoncus nisi."</p>
