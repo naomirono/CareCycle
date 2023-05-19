90a3cd18ae5c55d678f725bbf65e8d7559fd4de0
Naomi Cherotich <naomirono1454@gmail.com>
2023-05-20 01:33:13 +0300

    Add more navbar links

--- Client/src/components/NavbarLanding.jsx
+++ Client/src/components/NavbarLanding.jsx
@@ -19,13 +19,16 @@ const NavbarLanding = () => {
                 Home
               </a>
               <a href="#" className="font-medium text-white hover:text-gray-100 px-3 py-2 rounded-md md:ml-8 text-[16px] md:my-0 my-10">
-                About Us 
+                About 
               </a>
               <a href="#" className="font-medium text-white hover:text-gray-100 px-3 py-2 rounded-md md:ml-8 text-[16px] md:my-0 my-10">
-                Events
+                Impact 
               </a>
               <a href="#" className="font-medium text-white hover:text-gray-100 px-3 py-2 rounded-md md:ml-8 text-[16px] md:my-0 my-10">
-                Shop
+                Contact
+              </a>
+              <a href="#" className="font-medium text-white hover:text-gray-100 px-3 py-2 rounded-md md:ml-8 text-[16px] md:my-0 my-10">
+                FAQ
               </a>
             </div>
             <Button>Donate Now</Button>
