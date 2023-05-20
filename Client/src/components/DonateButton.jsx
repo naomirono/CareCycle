06f36a9d769f49ea94e61ad0e7f744de08648a13
Naomi Cherotich <naomirono1454@gmail.com>
2023-05-20 17:03:55 +0300

    Update code to react

--- Client/src/components/DonateButton.jsx
+++ Client/src/components/DonateButton.jsx
@@ -1,1 +1,39 @@
-import { loadStripe } from '@stripe/stripe-js';
+import { useState } from 'react';
+
+const DonateButton = () => {
+  const [loading, setLoading] = useState(false);
+
+  const handleDonateClick = async () => {
+    setLoading(true);
+
+    const stripe = await stripePromise;
+
+    const response = await fetch('/create-checkout-session', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const session = await response.json();
+
+    const result = await stripe.redirectToCheckout({
+      sessionId: session.id,
+    });
+
+    if (result.error) {
+      console.error(result.error.message);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <button
+      className="bg-white text-[#7B1FA2] font-semibold py-3 px-6 rounded-full shadow-lg transition-colors duration-300"
+      onClick={handleDonateClick}
+      disabled={loading}
+    >
+      {loading ? 'Processing...' : 'Donate Now'}
+    </button>
+  );
+};
