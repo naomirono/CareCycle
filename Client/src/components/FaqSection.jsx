2e68cb654e84f4cf994eadb708ca7886deb31c8f
Naomi Cherotich <naomirono1454@gmail.com>
2023-05-24 23:47:44 +0300

    Update code to be responsive

--- Client/src/components/FaqSection.jsx
+++ Client/src/components/FaqSection.jsx
@@ -56,42 +56,43 @@ const FAQSection = () => {
 
   return (
     <section className="bg-gray-100 py-16">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 max-w-[1200px]">
-        <div className="lg:text-center">
-          <h2 className="text-base text-[#7B1FA2] font-semibold tracking-wide uppercase">Frequently Asked Questions</h2>
-          <p className="mt-2 leading-6 text-3xl font-extrabold text-gray-900">Have a question? We have answers.</p>
-        </div>
-        <div className="mt-12">
-          {visibleFaqs.map((faq, index) => (
-            <div
-              key={index}
-              className="py-2"
-              onMouseLeave={handleQuestionMouseLeave}
-            >
-              <button
-                className="text-lg leading-6 font-medium text-gray-900 hover:text-[#7B1FA2] focus:outline-none transition duration-150 ease-in-out"
-                onClick={() => handleQuestionClick(index)}
-              >
-                {faq.question}
-              </button>
-              {activeIndex === index && (
-                <p className="mt-2 text-base text-gray-500">{faq.answer}</p>
-              )}
-            </div>
-          ))}
-          {!showAll && (
-            <div className="mt-8 text-center">
-              <button
-                className="bg-[#7B1FA2] text-white px-6 py-3 rounded-md text-base font-medium hover:bg-blue-600 focus:outline-none focus:bg-indigo-500"
-                onClick={handleReadMoreClick}
-              >
-                Read More
-              </button>
-            </div>
-          )}
-        </div>
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 max-w-[1200px]">
+      <div className="text-center">
+        <h2 className="text-base text-[#7B1FA2] font-semibold tracking-wide uppercase">Frequently Asked Questions</h2>
+        <p className="mt-2 leading-6 text-3xl font-extrabold text-gray-900">Have a question? We have answers.</p>
       </div>
-    </section>
+      <div className="mt-12">
+        {visibleFaqs.map((faq, index) => (
+          <div
+            key={index}
+            className="py-2"
+            onMouseLeave={handleQuestionMouseLeave}
+          >
+            <button
+              className="text-lg leading-6 font-medium text-gray-900 hover:text-[#7B1FA2] focus:outline-none transition duration-150 ease-in-out text-left w-full"
+              onClick={() => handleQuestionClick(index)}
+            >
+              {faq.question}
+            </button>
+            {activeIndex === index && (
+              <p className="mt-2 text-base text-gray-500">{faq.answer}</p>
+            )}
+          </div>
+        ))}
+        {!showAll && (
+          <div className="mt-8 text-center">
+            <button
+              className="bg-[#7B1FA2] text-white px-6 py-3 rounded-md text-base font-medium hover:bg-blue-600 focus:outline-none focus:bg-indigo-500"
+              onClick={handleReadMoreClick}
+            >
+              Read More
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  </section>
+  
   );
 };
 
