55aea378af0522d64b086879d30e78da609d8f10
Naomi Cherotich <naomirono1454@gmail.com>
2023-05-31 23:39:37 +0300

    Add padding on the small devices

--- Client/src/components/Testimonial.jsx
+++ Client/src/components/Testimonial.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const TestimonialsSection = () => {
   return (
-    <section className="py-16 bg-gradient-to-l from-[#dc2430] to-[#7B1FA2]">
+    <section className="py-16 bg-gradient-to-l from-[#dc2430] to-[#7B1FA2] px-4 sm:px-8">
       <div className="container max-w-[1200px] mx-auto">
         <h2 className="text-3xl md:text-4xl font-bold text-center mb-6 text-white">Testimonials</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
