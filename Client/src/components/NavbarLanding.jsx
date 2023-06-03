89491d75efefc564f899ccc558a97d7d40bed8f9
Naomi Cherotich <naomirono1454@gmail.com>
2023-06-03 10:28:20 +0300

    Update the code to meke it responsive

--- Client/src/components/NavbarLanding.jsx
+++ Client/src/components/NavbarLanding.jsx
@@ -1,78 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DonateButton from './DonateButton';
 import Logo from '../assets/carelogo1.png';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 
 const NavbarLanding = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div>
-    <header className="bg-gradient-to-l from-[#dc2430] to-[#7B1FA2]">
-      <nav className="max-w-7xl mx-auto pt-2 pb-2 px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
-          <div className="flex items-center">
-            <div className="flex-shrink-0">
-              <img className="h-12 w-18 sm:h-16 sm:w-22" src={Logo} alt="Logo" />
+      <header className="bg-gradient-to-l from-[#dc2430] to-[#7B1FA2] relative">
+        <nav className="max-w-7xl mx-auto pt-2 pb-2 px-4 sm:px-6 lg:px-8">
+          <div className="flex flex-col sm:flex-row items-center justify-between">
+            <div className="flex items-center relative z-10">
+              <div className="flex-shrink-0">
+                <img className="h-12 w-18 sm:h-16 sm:w-22" src={Logo} alt="Logo" />
+              </div>
+              <div
+                onClick={toggleMenu}
+                className="text-white focus:outline-none text-3xl md:hidden flex items-center ml-12 mt-2"
+              >
+                {isMenuOpen ? <AiOutlineClose /> : <AiOutlineMenu />}
+              </div>
+            </div>
+            <div
+              className={`${
+                isMenuOpen ? 'block' : 'hidden'
+              } mt-4 sm:mt-0 md:flex md:items-center bg-black bg-opacity-90 md:bg-transparent absolute md:relative top-full md:top-auto left-0 w-full md:w-auto md:z-0 z-20 md:pt-0 py-4 transition-all duration-500 ease-in text-center `}
+            >
+              <a
+                href="#home"
+                onClick={toggleMenu}
+                className="block font-medium text-white hover:text-gray-100 px-6 py-6 rounded-md text-lg "
+              >
+                Home
+              </a>
+              <a
+                href="#about"
+                onClick={toggleMenu}
+                className="block font-medium text-white hover:text-gray-100 px-6 py-6 rounded-md text-lg"
+              >
+                About
+              </a>
+              <a
+                href="#impact"
+                onClick={toggleMenu}
+                className="block font-medium text-white hover:text-gray-100 px-6 py-6 rounded-md text-lg"
+              >
+                Impact
+              </a>
+              <a
+                href="#contact"
+                onClick={toggleMenu}
+                className="block font-medium text-white hover:text-gray-100 px-6 py-6 rounded-md text-lg"
+              >
+                Contact
+              </a>
+              <a
+                href="#FAQ"
+                onClick={toggleMenu}
+                className="block font-medium text-white hover:text-gray-100 px-6 py-6 rounded-md text-lg"
+              >
+                FAQ
+              </a>
+            </div>
+            <div className="md:block hidden">
+              <DonateButton />
             </div>
           </div>
-          <div className="md:hidden flex items-center">
-            <button className="text-white focus:outline-none">
-              <svg
-                className="h-8 w-8"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            </button>
-          </div>
-          <div className="hidden md:block md:flex md:items-center md:pb-0 pb-14 absolute md:static md:z-auto left-0 w-full md:w-auto md-pl-0 pl-9 transition-all duration-500 ease-in">
-            <a
-              href="#home"
-              className="font-medium text-white hover:text-gray-100 px-3 py-2 rounded-md md:ml-8 text-[16px] md:my-0 my-10"
-            >
-              Home
-            </a>
-            <a
-              href="#about"
-              className="font-medium text-white hover:text-gray-100 px-3 py-2 rounded-md md:ml-8 text-[16px] md:my-0 my-10"
-            >
-              About
-            </a>
-            <a
-              href="#impact"
-              className="font-medium text-white hover:text-gray-100 px-3 py-2 rounded-md md:ml-8 text-[16px] md:my-0 my-10"
-            >
-              Impact
-            </a>
-            <a
-              href="#contact"
-              className="font-medium text-white hover:text-gray-100 px-3 py-2 rounded-md md:ml-8 text-[16px] md:my-0 my-10"
-            >
-              Contact
-            </a>
-            <a
-              href="#FAQ"
-              className="font-medium text-white hover:text-gray-100 px-3 py-2 rounded-md md:ml-8 text-[16px] md:my-0 my-10"
-            >
-              FAQ
-            </a>
-          </div>
-          <div className="md:block hidden">
-            <DonateButton />
-          </div>
-        </div>
-      </nav>
-    </header>
-  </div>
-  
-  
-  )
-}
+        </nav>
+      </header>
+    </div>
+  );
+};
 
-export default NavbarLanding
+export default NavbarLanding;
